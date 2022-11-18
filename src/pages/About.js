1908522a894e6542ff712bcafe860d1ee7ffa4aa
Moustapha Camara <cmr.mous@gmail.com>
2022-11-18 16:16:09 +0100

    v1

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quia, ipsa
+        laudantium nobis obcaecati blanditiis atque corporis illo, incidunt
+        cupiditate repudiandae ab sit voluptatem rem culpa eum molestiae dolore
+        earum, provident at sunt odit quaerat facilis exercitationem! Similique,
+        natus vel.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt unde
+        consequatur, sit recusandae voluptates laboriosam veniam odit,
+        repudiandae esse harum ea cumque voluptatem, qui omnis enim dolorem
+        soluta? Culpa omnis laborum aperiam aspernatur praesentium corrupti ex!
+        Asperiores repudiandae quae delectus sint fugit placeat, sequi, quam
+        cumque excepturi nostrum veritatis quaerat.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem,
+        cupiditate cumque repellendus beatae, officiis sequi incidunt tempora
+        dolorem aperiam ea error saepe nostrum sunt, laboriosam eligendi.
+        Placeat aspernatur quaerat ullam sint est? Voluptatem ratione sunt
+        aspernatur rem! Fuga, nisi sed. Natus incidunt animi omnis reiciendis
+        nulla veritatis odio ea voluptatibus doloremque est ex, cumque laborum
+        repudiandae vel tempora recusandae voluptas!
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Error404.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>Error404</h1>
+      <h2>Oups.. i think something wrong happened.</h2>
+      <a href="/">Back to home</a>
+    </div>
+  );
+};
+
+export default Error404;
